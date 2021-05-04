22e5e0d7c277b364b2955926dbf7a6e1bbd5676a
Pablo <pablo.martin.lopez@ieslaarboleda.es>
2021-05-04 20:10:08 +0200

    navbar comun

--- formularios/header.php
+++ formularios/header.php
@@ -1,12 +1,41 @@
-<?php
-session_start();
-?>
-
 <nav class="navbar navbar-expand-lg navbar-dark navbar-custom fixed-top">
     <div class="container">
         <a class="navbar-brand" href="../index.html">Piñata Feliz</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
         <div class="collapse navbar-collapse" id="navbarResponsive">
+
+            <?php
+            if (isset($_SESSION['tipo'])) {
+                if ($_SESSION['tipo'] == "admin") {
+            ?>
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item"><a class="nav-link" href="../">Crear BD</a></li>
+                    </ul>
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item"><a class="nav-link" href="../">Borar BD</a></li>
+                    </ul>
+
+                <?php
+                } else {
+                    echo "Usuario";
+                }
+                ?>
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item"><a class="nav-link" href="../">Cerrar sesion</a></li>
+                </ul>
+            <?php
+            } else {
+            ?>
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item"><a class="nav-link" href="../formularios/loginUsuario.php">SIGN UP</a></li>
+                </ul>
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item"><a class="nav-link" href="../formularios/altaUsuario.php">LOG IN</a></li>
+                </ul>
+            <?php
+            }
+            ?>
+
             <ul class="navbar-nav ml-auto">
                 <li class="nav-item"><a class="nav-link" href="../">Volver</a></li>
             </ul>
--- formularios/loginUsuario.php
+++ formularios/loginUsuario.php
@@ -1,3 +1,6 @@
+<?php
+    session_start();
+?>
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -16,18 +19,9 @@
         <link href="../css/styles.css" rel="stylesheet" />
     </head>
     <body>
-        <!-- Navigation-->
-        <nav class="navbar navbar-expand-lg navbar-dark navbar-custom fixed-top">
-            <div class="container">
-                <a class="navbar-brand" href="../index.html">Start Bootstrap</a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
-                <div class="collapse navbar-collapse" id="navbarResponsive">
-                    <ul class="navbar-nav ml-auto">
-                        <li class="nav-item"><a class="nav-link" href="../">Volver</a></li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
+        <?php
+            include("header.php");
+        ?>
         <header class="masthead text-center text-white">
             <div class="masthead-content">
                 <div class="container d-flex  flex-wrap justify-content-around ">
