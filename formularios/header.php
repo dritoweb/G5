114684a204639b0de0f9ebd6d9340b05dac8de2c
Pablo <pablo.martin.lopez@ieslaarboleda.es>
2021-05-04 19:53:57 +0200

    actualizar navbar

---
+++ formularios/header.php
@@ -0,0 +1,15 @@
+<?php
+session_start();
+?>
+
+<nav class="navbar navbar-expand-lg navbar-dark navbar-custom fixed-top">
+    <div class="container">
+        <a class="navbar-brand" href="../index.html">Piñata Feliz</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item"><a class="nav-link" href="../">Volver</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
