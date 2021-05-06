805367e165e6fbf63766ad40f45f0f99896f5e9b
FONJOKER <julian.fagadau@ieslaarboleda.es>
2021-05-06 16:56:21 +0200

    He actualizado los estilos del menu

--- formularios/header.php
+++ formularios/header.php
@@ -2,53 +2,53 @@
     <div class="container">
         <a class="navbar-brand" href="../">Piñata Feliz</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
-        <div class="collapse navbar-collapse" id="navbarResponsive">
+        <div class="collapse navbar-collapse  row d-flex justify-content-center" id="navbarResponsive">
 
             <?php
             if (isset($_SESSION['tipo'])) {
                 if ($_SESSION['tipo'] == "admin") {
             ?>
-                    <ul class="navbar-nav ml-auto">
-                        <li class="nav-item"><a class="nav-link" href="../">Crear BD</a></li>
-                    </ul>
-                    <ul class="navbar-nav ml-auto">
-                        <li class="nav-item"><a class="nav-link" href="../">Borar BD</a></li>
-                    </ul>
                     <div class="dropdown">
+                            <a class="btn dropdown nav-link text-white" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown"  aria-expanded="false">
+                                Manejar BD
+                            </a>
+                            <div class="dropdown-menu">
+                                <a class="nav-link" href="../codigo/principal.php?crearbd">Crear BD</a>
+                                <a class="nav-link" href="../codigo/principal.php?delbd">Borar BD</a>
+                                <a class="nav-link" href="../formularios/altaAnimador.php">Alta Animador</a>
+                                <a class="nav-link" href="../formularios/bajaAnimador.php">Eliminar Animador</a>
+                            </div>
+                        </div>
+                        <div class="dropdown">
                             <a class="btn dropdown nav-link text-white" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown"  aria-expanded="false">
                                 Consultas
                             </a>
                             <div class="dropdown-menu">
-                                <a class="dropdown-item nav-link" href="../codigo/principal.php?consultaespecialidad">Todas las especialidades</a>
-                                <a class="dropdown-item nav-link" href="../codigo/principal.php?consultafiestas">Todas las fiestas</a>
-                                <a class="dropdown-item nav-link" href="../formularios/fiestaUsuario.php">Fiestas por clientes</a>
+                                <a class="nav-link" href="../codigo/principal.php?consultaespecialidad">Todas las especialidades</a>
+                                <a class=" nav-link" href="../codigo/principal.php?consultafiestas">Todas las fiestas</a>
+                                <a class=" nav-link" href="#">Fiestas por clientes</a>
                             </div>
-                    </div>
+                        </div>
 
                 <?php
                 } else {
-                    echo "Usuario";
+                    ?>
+                            <a class="btn dropdown nav-link text-white" href="../formularios/SolicitarFiesta.php">Solicitar Fiesta</a>
+                            <a class="btn dropdown nav-link text-white" href="../codigo/principal.php?consultarfiesta">Consultar mis fiestas</a>
+                    <?php
                 }
                 ?>
-                <ul class="navbar-nav ml-auto">
-                    <li class="nav-item"><a class="nav-link" href="../">Cerrar sesion</a></li>
-                </ul>
+                <a class="btn dropdown nav-link text-white" href="../codigo/principal.php?cerrarsesion">Cerrar sesion</a>
             <?php
             } else {
             ?>
-                <ul class="navbar-nav ml-auto">
-                    <li class="nav-item"><a class="nav-link" href="../formularios/loginUsuario.php">SIGN UP</a></li>
-                </ul>
-                <ul class="navbar-nav ml-auto">
-                    <li class="nav-item"><a class="nav-link" href="../formularios/altaUsuario.php">LOG IN</a></li>
-                </ul>
+                <a class="btn dropdown nav-link text-white" href="../formularios/altaUsuario.php">Registrarse</a>
+                <a class="btn dropdown nav-link text-white" href="../formularios/loginUsuario.php">Iniciar Sesion</a>
             <?php
             }
             ?>
 
-            <ul class="navbar-nav ml-auto">
-                <li class="nav-item"><a class="nav-link" href="../">Volver</a></li>
-            </ul>
+                <a class="btn dropdown nav-link text-white" href="../">Volver</a>
         </div>
     </div>
 </nav>
--- index.php
+++ index.php
@@ -23,54 +23,56 @@ session_start();
 
 <body>
     <!-- Navigation-->
-    <nav class="navbar navbar-expand-lg navbar-dark navbar-custom fixed-top">
-        <div class="container">
+    <nav class="navbar navbar-expand-lg navbar-dark navbar-custom fixed-top d-flex justify-content-center ">
+        <div class="container ">
             <a class="navbar-brand" href="#">Piñata Feliz</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
-            <div class="collapse navbar-collapse" id="navbarResponsive">
+            <div class="collapse navbar-collapse row d-flex justify-content-center " id="navbarResponsive">
 
                 <?php
                 if (isset($_SESSION['tipo'])) {
                     if ($_SESSION['tipo'] == "admin") {
                 ?>
-                        <ul class="navbar-nav ml-auto">
-                            <li class="nav-item"><a class="nav-link" href="codigo/principal.php?crearbd">Crear BD</a></li>
-                            <li class="nav-item"><a class="nav-link" href="codigo/principal.php?delbd">Borar BD</a></li>
-                            <li class="nav-item"><a class="nav-link" href="formularios/altaAnimador.php">Alta Animador</a></li>
-                            <li class="nav-item"><a class="nav-link" href="formularios/bajaAnimador.php">Eliminar Animador</a></li>
-                        </ul>
+                        
+                        <div class="dropdown">
+                            <a class="btn dropdown nav-link text-white" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown"  aria-expanded="false">
+                                Manejar BD
+                            </a>
+                            <div class="dropdown-menu">
+                                <a class="nav-link" href="codigo/principal.php?crearbd">Crear BD</a>
+                                <a class="nav-link" href="codigo/principal.php?delbd">Borar BD</a>
+                                <a class="nav-link" href="formularios/altaAnimador.php">Alta Animador</a>
+                                <a class="nav-link" href="formularios/bajaAnimador.php">Eliminar Animador</a>
+                            </div>
+                        </div>
                         <div class="dropdown">
                             <a class="btn dropdown nav-link text-white" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown"  aria-expanded="false">
                                 Consultas
                             </a>
                             <div class="dropdown-menu">
-                                <a class="dropdown-item nav-link" href="codigo/principal.php?consultaespecialidad">Todas las especialidades</a>
-                                <a class="dropdown-item nav-link" href="codigo/principal.php?consultafiestas">Todas las fiestas</a>
-                                <a class="dropdown-item nav-link" href="#">Fiestas por clientes</a>
+                                <a class="nav-link" href="codigo/principal.php?consultaespecialidad">Todas las especialidades</a>
+                                <a class=" nav-link" href="codigo/principal.php?consultafiestas">Todas las fiestas</a>
+                                <a class=" nav-link" href="#">Fiestas por clientes</a>
                             </div>
                         </div>
 
                     <?php
                     } else {
                         ?>
-                            <ul class="navbar-nav ml-auto">
-                            <li class="nav-item"><a class="nav-link" href="formularios/SolicitarFiesta.php">Solicitar Fiesta</a></li>
-                            <li class="nav-item"><a class="nav-link" href="codigo/principal.php?consultarfiesta">Consultar mis fiestas</a></li>
-                        </ul>
+                            <a class="btn dropdown nav-link text-white" href="formularios/SolicitarFiesta.php">Solicitar Fiesta</a>
+                            <a class="btn dropdown nav-link text-white" href="codigo/principal.php?consultarfiesta">Consultar mis fiestas</a>
                         <?php
                     }
                     ?>
-                    <ul class="navbar-nav ml-auto">
-                        <li class="nav-item"><a class="nav-link" href="codigo/principal.php?cerrarsesion">Cerrar sesion</a></li>
-                    </ul>
+                    
+                        <a class="btn dropdown nav-link text-white" href="codigo/principal.php?cerrarsesion">Cerrar sesion</a>
+                    
                 <?php
                 } else {
                 ?>
                     
-                    <ul class="navbar-nav ml-auto">
-                        <li class="nav-item"><a class="nav-link" href="formularios/altaUsuario.php">SIGN UP</a></li>
-                        <li class="nav-item"><a class="nav-link" href="formularios/loginUsuario.php">LOG IN</a></li>
-                    </ul>
+                        <a class="btn dropdown nav-link text-white" href="formularios/altaUsuario.php">Registrarse</a>
+                        <a class="btn dropdown nav-link text-white" href="formularios/loginUsuario.php">Iniciar Sesion</a>
                 <?php
                 }
                 ?>
