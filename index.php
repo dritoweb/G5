85514033f5fb96a4059f9d1e6e9dfb68e03c3a91
irene-afk <irene.dacosta.coballes@ieslaarboleda.es>
2021-05-06 17:55:42 +0200

    retoque de interfaz

--- index.php
+++ index.php
@@ -82,8 +82,8 @@ session_start();
         
         <div class="masthead-content">
             <div class="container">
-                <h1 class="masthead-heading mb-0">One Page Wonder</h1>
-                <h2 class="masthead-subheading mb-0">Will Rock Your Socks Off</h2>
+                <h1 class=" mb-0">Para una experiencia inolvidable..</h1>
+                <h2 class=" mb-0">Tus fiestas a nuestro servicio</h2>
                 <a class="btn btn-primary btn-xl rounded-pill mt-5" href="#!">Learn More</a>
             </div>
         </div>
@@ -100,7 +100,7 @@ session_start();
                 </div>
                 <div class="col-lg-6 order-lg-1">
                     <div class="p-5">
-                        <h2 class="display-4">For those about to rock...</h2>
+                        <h2 class="display-4">Aniversarios en la playa!</h2>
                         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod aliquid, mollitia odio veniam sit iste esse assumenda amet aperiam exercitationem, ea animi blanditiis recusandae! Ratione voluptatum molestiae adipisci, beatae obcaecati.</p>
                     </div>
                 </div>
@@ -115,12 +115,13 @@ session_start();
                 </div>
                 <div class="col-lg-6">
                     <div class="p-5">
-                        <h2 class="display-4">We salute you!</h2>
+                        <h2 class="display-4">Cumpleaños!</h2>
                         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod aliquid, mollitia odio veniam sit iste esse assumenda amet aperiam exercitationem, ea animi blanditiis recusandae! Ratione voluptatum molestiae adipisci, beatae obcaecati.</p>
                     </div>
                 </div>
             </div>
         </div>
+        
     </section>
     <section>
         <div class="container">
@@ -130,12 +131,14 @@ session_start();
                 </div>
                 <div class="col-lg-6 order-lg-1">
                     <div class="p-5">
-                        <h2 class="display-4">Let there be rock!</h2>
+                        <h2 class="display-4">Bodas!</h2>
                         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod aliquid, mollitia odio veniam sit iste esse assumenda amet aperiam exercitationem, ea animi blanditiis recusandae! Ratione voluptatum molestiae adipisci, beatae obcaecati.</p>
                     </div>
                 </div>
             </div>
         </div>
+        
+    </header>
     </section>
     <!-- Footer-->
     <footer class="py-5 bg-black">
